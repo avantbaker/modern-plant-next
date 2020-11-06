2c777f8af7e47921409cdfbaa6e22be34cac884b
Avant Baker <avantbaker@Avants-MacBook-Pro.local>
2020-11-06 10:22:28 -0500

    Updated homepage copy

--- components/home/grid-gallery-section.js
+++ components/home/grid-gallery-section.js
@@ -1,17 +1,23 @@
-import React from 'react'
+import React from 'react';
 import GridGallery from '../grid-gallery';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 export default function GridGallerySection() {
-    return (
-        <div className="grid-gallery-section">
-            <div className='grid-gallery-section__container container _80'>
-                <FontAwesomeIcon className="grid-gallery-section__icon heading-2" icon={faInstagram} />
-                <h3 className="heading-3">More of the good stuff.</h3>
-                <h5 className="heading-5 mb-xxl">Follow us. We're bold purveyors of <br />wellness, and a proud modern apothecary </h5>
-            </div>
-            <GridGallery />
-        </div>
-    )
+	return (
+		<div className="grid-gallery-section">
+			<div className="grid-gallery-section__container container _80">
+				<FontAwesomeIcon
+					className="grid-gallery-section__icon heading-2"
+					icon={faInstagram}
+				/>
+				<h3 className="heading-3">More of the good stuff.</h3>
+				<h5 className="heading-5 mb-xxl">
+					Stay Connected. <br />
+					Be A Part of Our Community.
+				</h5>
+			</div>
+			<GridGallery />
+		</div>
+	);
 }
