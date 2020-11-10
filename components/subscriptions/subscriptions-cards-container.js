d2a6d02ab093652861768b3fa0bc3d41bd2ca47f
Avant Baker <avantbaker@Avants-MBP.attlocal.net>
2020-11-09 23:40:11 -0500

    Updated the rest of the copy on the subscriptions page

--- components/subscriptions/subscriptions-cards-container.js
+++ components/subscriptions/subscriptions-cards-container.js
@@ -1,57 +1,56 @@
 import SubscriptionCard from '../subscription-card';
 
 const cardData = [
-  {
-      imageSrc: '/images/global/greenbottle.png',
-      title: 'Lorem ipsum dolor',
-      info: 'Upload your Diagnomics DNA Data.. Lorem ipsum dolor sit amet, consectetur adipiscing',
-      price: '$199.99',
-      term: '12 Months',
-      attributes: [
-        { text: '1 box per month'},
-        { text: 'Pair with DNA test kit'}
-      ],
-      buttonText: 'Add to Cart'
-  },
-  {
-      imageSrc: '/images/global/boxlove.png',
-      title: 'Lorem ipsum dolor',
-      info: 'Upload your Diagnomics DNA Data.. Lorem ipsum dolor sit amet, consectetur adipiscing',
-      price: '$149.99',
-      term: '12 Months',
-      attributes: [
-        { text: '2 box per month'},
-        { text: 'mix & match terpenes'},
-        { text: 'Pair with dna test kit'}
-      ],
-      buttonText: 'Add to Cart'
-  },
-  {
-      imageSrc: '/images/global/milk.png',
-      title: 'Lorem ipsum dolor',
-      info: 'Upload your Diagnomics DNA Data.. Lorem ipsum dolor sit amet, consectetur adipiscing',
-      price: '$299.99',
-      term: '18 Months',
-      attributes: [
-        { text: '4 box per month'},
-        { text: 'mix & match terpenes'},
-        { text: 'Pair with dna test kit'},
-        { text: 'Birthday box extra'},
-      ],
-      buttonText: 'Add to Cart'
-  }
+	{
+		imageSrc: '/images/global/greenbottle.png',
+		title: 'Subscribe w/o DNA Testing',
+		info: 'Hello Health!',
+		price: '$99',
+		term: '1 Month',
+		attributes: [{ text: '1 box a month' }],
+		buttonText: 'Subscribe Today',
+	},
+	{
+		imageSrc: '/images/updates/windchime.jpg',
+		title: 'Subscribe w/ DNA Testing',
+		info: 'Custom Crafted For You',
+		price: '$79.99',
+		term: '1 Month',
+		attributes: [
+			{ text: '1 box per month' },
+			{ text: 'mix & match terpenes' },
+			{ text: 'Pair with dna test kit' },
+		],
+		buttonText: 'Subscribe Today',
+	},
+	// {
+	// 	imageSrc: '/images/global/milk.png',
+	// 	title: 'Lorem ipsum dolor',
+	// 	info:
+	// 		'Upload your Diagnomics DNA Data.. Lorem ipsum dolor sit amet, consectetur adipiscing',
+	// 	price: '$299.99',
+	// 	term: '18 Months',
+	// 	attributes: [
+	// 		{ text: '4 box per month' },
+	// 		{ text: 'mix & match terpenes' },
+	// 		{ text: 'Pair with dna test kit' },
+	// 		{ text: 'Birthday box extra' },
+	// 	],
+	// 	buttonText: 'Add to Cart',
+	// },
 ];
 
 const SubscriptionsCardsContainer = () => {
-  return (
-    <div className="subscriptions-cards subscription-options container _80">
-        <h4 className='heading-3 mb-xxl'>Subscribe to Regular Wellness</h4>
-        <div className='subscription-options__cards'>{
-            cardData.map((card, i) => <SubscriptionCard type="secondary" {...card} />)
-        }</div>
-    </div>
-  )
-}
+	return (
+		<div className="subscriptions-cards subscription-options container _80">
+			<h4 className="heading-3 mb-xxl">Subscribe to Regular Wellness</h4>
+			<div className="subscription-options__cards">
+				{cardData.map((card, i) => (
+					<SubscriptionCard type="secondary" {...card} />
+				))}
+			</div>
+		</div>
+	);
+};
 
-
-export default SubscriptionsCardsContainer
+export default SubscriptionsCardsContainer;
